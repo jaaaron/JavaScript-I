ce45f4943165e25bfe67860790c71062c029356b
james <xjamesaaron@yahoo.com>
2019-09-17 13:23:21 -0700

    extraReps.js

---
+++ extraReps.js
@@ -0,0 +1,5 @@
+// Create an object:
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log(person);
+
+console.log(person.firstName + " is " + person.age + " years old.");
